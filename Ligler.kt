1850074af5664fdd23570d2c726993a56704cfa9
Furkan ÇAĞLAR <70340215+furkancaglarr@users.noreply.github.com>
2022-04-14 22:50:30 +0300

    new files add

---
+++ Ligler.kt
@@ -0,0 +1,34 @@
+class Ligler {
+
+    var ıngıltereLıg = mapOf<Int, String>(
+        1 to "Manchester City",
+        2 to "Liverpol",
+        3 to "Chelsa",
+        4 to "Arsenal",
+        5 to "Everton",
+        6 to "Tottenham",
+        7 to "Manchester United",
+        8 to "Southampton",
+        9 to "Newcastle United",
+        10 to "Stoke",
+        11 to "Crystal Palace",
+        12 to "West Ham",
+        13 to "Swansea",
+        14 to "Astan Villa",
+        15 to "Hull City",
+        16 to "West Brom",
+        17 to "Sunderland",
+        18 to "Norwich",
+        19 to "Fulham",
+        20 to "Cardiff"
+    )
+
+    var almanyaLıg = Array<String>(size = 5) { " " }
+
+
+
+}
+/*
+//1 to "Bayern München", 2 to , 3 to "Dortmund", 4 to "Wolfsburg", 5 to "Frakfurt",
+//6 to "Leverkusen", 7 to "Union Berlin", 8 to "Stuttgort", 9 to "Freiburg", 10 to "Koln"
+*/
---
+++ Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+
+    var ıngıltere = Ligler()
+    for (items in ıngıltere.ıngıltereLıg) {
+        println(items)
+    }
+    println()
+    var almanya = Ligler()
+    for (items in almanya.almanyaLıg) {
+        println(items)
+    }
+}
